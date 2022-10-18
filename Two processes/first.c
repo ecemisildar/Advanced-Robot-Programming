934996da9f668afd6b49dda3855c111b8a6ede35
ecemisildar <51851879+ecemisildar@users.noreply.github.com>
2022-10-18 13:52:16 +0200

    Create first.c

---
+++ Two processes/first.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h> 
+#include <fcntl.h> 
+#include <sys/stat.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <stdlib.h>
+  
+int main() 
+{
+    int fd; 
+   
+    char * myfifo = "/tmp/myfifo"; 
+    mkfifo(myfifo, 0666); 
+    
+    char * myfifo_1 = "/tmp/myfifo_1"; 
+    mkfifo(myfifo_1, 0666);
+  
+    char input_string[80];
+    char received_string[80];
+
+    while (1) 
+    { 
+      	/* opens a file to write*/
+        fd = open(myfifo, O_WRONLY); 
+
+        printf("Please, write two integer numbers, separated by commas (,), or q to quit\n");
+        /* to be sure that the previous is executed immediately */
+	fflush(stdout);
+        /* read a full input line */
+        fgets(input_string, 80 , stdin); 
+        /* writes it in to the file */
+        write(fd, input_string, strlen(input_string)+1); 
+        close(fd); 
+        
+        /* opens a file to read*/
+        fd = open(myfifo_1, O_RDONLY);
+        /* reads the string coming from the second */
+        read(fd, received_string, sizeof(received_string));
+        printf("%s",received_string);
+        close(fd);
+        
+               
+        /* if the first input char is q, exit  */
+        if (input_string[0] == 'q') exit(EXIT_SUCCESS) ;
+  
+    } 
+    return 0; 
+} 
